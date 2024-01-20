37b3725578c3d2a95ceb037950c0ba1afdbcac55
Elmer <s0227373691@gms.tku.edu.tw>
2024-01-20 18:14:14 +0800

    update compile version

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     hardhat: {
       chainId: 1337,
